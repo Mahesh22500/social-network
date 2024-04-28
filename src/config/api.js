f4469777a87711b2306731527a6c593fdf94f55b
Mahesh22500 <maheshsingh22500@gmail.com>
2024-04-28 08:31:36 +0530

    modified api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://social-network-server-production-9f35.up.railway.app";
+
 
 const jwtToken = localStorage.getItem("jwt");
 
